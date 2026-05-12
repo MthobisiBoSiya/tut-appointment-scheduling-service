cd75fc5d1a9f319bd079f55deb91c6426feda984
scongo <khozavictor426@gmail.com>
2026-05-12 17:48:53 +0200

    initial commit

--- src/test/java/co/za/tut/student/tut_appointment_scheduling_service/TutAppointmentSchedulingServiceApplicationTests.java
+++ src/test/java/co/za/tut/student/tut_appointment_scheduling_service/TutAppointmentSchedulingServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package co.za.tut.student.tut_appointment_scheduling_service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TutAppointmentSchedulingServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package co.za.tut.student.tut_appointment_scheduling_service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TutAppointmentSchedulingServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
