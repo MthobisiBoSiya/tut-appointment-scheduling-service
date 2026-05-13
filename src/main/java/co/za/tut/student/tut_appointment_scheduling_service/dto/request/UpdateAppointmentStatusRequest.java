234d606ca34bfd0596de2e6f720f1fdea09d7438
Samkelisiwe <samkelisiweshongwe69@gmail.com>
2026-05-13 11:00:29 +0200

    Update

---
+++ src/main/java/co/za/tut/student/tut_appointment_scheduling_service/dto/request/UpdateAppointmentStatusRequest.java
@@ -0,0 +1,8 @@
+package co.za.tut.student.tut_appointment_scheduling_service.dto.request;
+
+import lombok.Data;
+
+@Data
+public class UpdateAppointmentStatusRequest {
+    private String status;
+}
